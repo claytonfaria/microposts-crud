22bfd70a187ad08e87492f5f2d823a7b0b296ad8
claytonfaria <claytonfaria@gmail.com>
2020-08-16 14:27:53 +0800

    initial commit

---
+++ src/http.js
@@ -0,0 +1,47 @@
+class EasyHTTP {
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  };
+
+  // Make an HTTP POST Request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  };
+
+  // Make and HTTP PUT request
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  };
+
+  // Make and HTTP DELETE Request
+  async delete(url) {
+    await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+    const resData = await 'Resource Deleted';
+    return resData;
+  };
+};
+
+export const http = new EasyHTTP();
